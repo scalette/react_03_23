4f59cb784e00ad4e8044f542cc45a7d3a6be166b
scalette <furca7e@gmail.com>
2023-03-16 22:24:21 +0100

    train: react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,60 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {BrowserRouter} from 'react-router-dom';
+import App from './components/App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import PageNotExists from './components/page-not-exists/page-not-exists';
+import Contact, { loader as contactLoader,
+  action as contactAction} from './routes/contacts/contacts'
+import EditContact, {action as editAction, destroyAction} from './routes/edit/edit'
+import Index from "./routes/index";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <PageNotExists />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <PageNotExists />,
+        children: [
+          {
+            index: true,
+            element: <Index />,
+          },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>
+          },
+        ]
+      }
+
+    ]
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
